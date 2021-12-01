11c478509bc9d97d08d3358bd1a7b44c2e6c6bf1
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2021-12-01 14:48:26 -0500

    Update MaxSumSubArray.java

--- MaxSumSubArray.java
+++ MaxSumSubArray.java
@@ -6,7 +6,7 @@ public class MaxSumSubArray {
 		// TODO Auto-generated method stub
 		MaxSumSubArray m = new MaxSumSubArray();
 		int[] nums = {1};
-		System.out.println(m.whenAllNumsAreNeg(nums));
+		System.out.println(m.minSubArrayLen(nums));
 	}
 	//dynamic programming
 	//kadanes algorithm
@@ -15,18 +15,26 @@ public class MaxSumSubArray {
 //	Each time we get a positive-sum compare it with max_so_far and update max_so_far if it is greater than sum
 	//The maximum subarray problem is a task to find the series of contiguous elements with the maximum sum in any given array.
 	//below solution doesn't handle case where all array numbers are negative
+	
+	//extension to this problem is to find the start and end indices
 	 public int minSubArrayLen(int[] nums) {
-		 int max_so_far = Integer.MIN_VALUE, sum = 0;
+		 int max_so_far = Integer.MIN_VALUE, sum = 0, s = 0, start = 0, end = 0;
 	        for(int i = 0; i< nums.length; i++){
 	        	sum += nums[i];
 	        	if(nums.length == 1)
 	        		return nums[i];
 	            if(sum < 0) {
 	            	sum = 0;
+	            	s = i + 1;
 	            }
-	            if(max_so_far < sum)
+	            if(max_so_far < sum) {
 	            	max_so_far = sum;
+	            	start = s;
+	            	end = i;
+	            }
 	        }
+	        System.out.println(start);
+	        System.out.println(end);
 	        return max_so_far;
 	    }
 	 
