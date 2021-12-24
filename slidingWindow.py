b8ae90636e205852cb1da11bde170fa1c630667e
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2021-12-23 21:30:10 -0500

    Update slidingWindow.py

--- slidingWindow.py
+++ slidingWindow.py
@@ -30,5 +30,43 @@ def highestSumOpt(nums, k):
     
     return highest_sum
 
+##2pointers
+def highestSum2Pointers(nums, k):
+    n = len(nums)
+    if n < k:
+        return -1
+
+    left = 0
+    right = k - 1
+    highest_sum = float('-inf')
+    while(right < n):
+        current_sum = 0
+        current_sum += arr[left] + arr[left + 1] + arr[right]
+        left+= 1
+        right+= 1
+        highest_sum = max(highest_sum, current_sum)
+    
+    return highest_sum
+
+##2pointers
+def highestSum2Pointers1(nums, k):
+    n = len(nums)
+    if n < k:
+        return -1
+
+    left = 0
+    current_sum = 0 
+    right = 0
+    highest_sum = float('-inf')
+    while(right < n):
+        current_sum += arr[right]
+        right+=1
+        if right-left == k:
+            highest_sum = max(highest_sum, current_sum)
+            current_sum -= arr[left]
+            left+= 1
+    
+    return highest_sum
+
 arr = [6, 5, 8, 3, 2, 1]
-print(highestSumOpt(arr,3))
+print(highestSum2Pointers1(arr,3))
