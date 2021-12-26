62d2eafb742da8cd1d8274360099c574325d92b8
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2021-12-25 19:29:47 -0500

    Time: 100 ms (62.16%), Space: 18.1 MB (43.76%) - LeetHub

---
+++ group-anagrams/group-anagrams.py
@@ -0,0 +1,8 @@
+import collections
+from collections import defaultdict
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            ans[tuple(sorted(s))].append(s)
+        return ans.values()
