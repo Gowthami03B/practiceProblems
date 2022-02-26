a46b70deba35d7d616b3569f0619cf067d44ba39
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-02-26 18:17:25 -0500

    Time: 9 ms (34.29%), Space: 47.7 MB (79.13%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -4,12 +4,15 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         for(int n : nums){
              map.put(n, map.getOrDefault(n,0)+1);
-        }
-        for(int key : map.keySet()){
-            if(map.get(key) > maxTimes){
-                return key;
+            if(map.get(n) > maxTimes){
+                return n;
             }
         }
+        // for(int key : map.keySet()){
+        //     if(map.get(key) > maxTimes){
+        //         return key;
+        //     }
+        // }
        return -1; 
     }
 }
