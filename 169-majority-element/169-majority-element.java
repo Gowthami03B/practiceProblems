78646902f98a7456433f8fe25246739ad49a2238
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-02-26 18:42:52 -0500

    Time: 2 ms (83.58%), Space: 56.6 MB (21.66%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,18 +1,27 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int maxTimes = nums.length/2;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int n : nums){
-             map.put(n, map.getOrDefault(n,0)+1);
-            if(map.get(n) > maxTimes){
-                return n;
+       //  int maxTimes = nums.length/2;
+       //  Map<Integer, Integer> map = new HashMap<>();
+       //  for(int n : nums){
+       //       map.put(n, map.getOrDefault(n,0)+1);
+       //      if(map.get(n) > maxTimes){
+       //          return n;
+       //      }
+       //  }
+       //  // for(int key : map.keySet()){
+       //  //     if(map.get(key) > maxTimes){
+       //  //         return key;
+       //  //     }
+       //  // }
+       // return -1; 
+        int count = 0;
+        int ele = 0;
+        for(int num : nums){
+            if(count == 0){
+                ele = num;
             }
+            count += (ele == num) ? 1 : -1;
         }
-        // for(int key : map.keySet()){
-        //     if(map.get(key) > maxTimes){
-        //         return key;
-        //     }
-        // }
-       return -1; 
+        return ele;
     }
 }
