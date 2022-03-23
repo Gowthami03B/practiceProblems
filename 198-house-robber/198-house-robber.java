ecdbfc5474ab5cff7fa8a66f8c73ae0f713fc874
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-03-23 13:41:23 -0400

    Time: 0 ms (100.00%), Space: 39.5 MB (82.01%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -17,12 +17,16 @@ class Solution {
         return nums[0];
     if(nums.length == 2)
         return Math.max(nums[0], nums[1]);
+    int[] dp = new int[nums.length];
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    //using only 2 variables
     int rob1 = nums[0], rob2 = Math.max(nums[0], nums[1]);
     for(int i = 2; i < nums.length; i++){
-        int maxProfit = Math.max(nums[i] + rob1, rob2);
-        rob1 = rob2;
-        rob2 = maxProfit;
+        dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        // rob1 = rob2;
+        // rob2 = maxProfit;
     }
-    return rob2;
+    return dp[nums.length - 1];
 }
 }
