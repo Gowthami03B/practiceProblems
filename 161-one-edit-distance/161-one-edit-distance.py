b00718ef266bd311ba282e539113619f7ccbda55
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-04-03 19:24:20 -0400

    Update 161-one-edit-distance.py

--- 161-one-edit-distance/161-one-edit-distance.py
+++ 161-one-edit-distance/161-one-edit-distance.py
@@ -13,3 +13,21 @@ class Solution:
                     return s[i:] == t[i+1:]
             i += 1
         return True
+    #solution that saves space
+#     def isOneEditDistance(self, s, t):
+#         if abs(len(s) - len(t)) > 1 or s == t:
+#             return False
+        
+#         found_inequality = False
+#         i = j = 0
+        
+#         while i < len(s) and j < len(t):
+#             if s[i] != t[j]:
+#                 if found_inequality: return False
+#                 found_inequality = True
+#                 if len(s) < len(t): i -= 1
+#                 elif len(s) > len(t): j -= 1
+#             i += 1
+#             j += 1
+        
+#         return True
