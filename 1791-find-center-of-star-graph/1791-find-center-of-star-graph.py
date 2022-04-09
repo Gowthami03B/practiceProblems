24d02898bc57964e955c8fd4a137d5ed8d53b61d
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-04-09 10:16:51 -0400

    Time: 828 ms (92.62%), Space: 49.9 MB (64.96%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,5 +1,7 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
+#         #by creating adjacency lists in dict to store edges
+#         #O(N+E), O(N)
 #         graph = defaultdict(list)
 #         for edge in edges:
 #             graph[edge[0]].append(graph[edge[1]])
@@ -9,16 +11,19 @@ class Solution:
 #             if len(value) == len(edges):
 #                 return key
 #         return -1
-    
-        # degree = [0 for i in range(len(edges) + 2)]
-        # for a,b in edges:
-        #     degree[a] += 1
-        #     degree[b] += 1
-        # for i in range(1, len(degree)):
-        #     if degree[i] == len(edges):
-        #         return i
-        # return -1
         
+#         #using array to count no of edges to each node
+#         #O(N+E), O(N)
+#         degree = [0 for i in range(len(edges) + 2)]
+#         for a,b in edges:
+#             degree[a] += 1
+#             degree[b] += 1
+#         for i in range(1, len(degree)):
+#             if degree[i] == len(edges):
+#                 return i
+#         return -1
+        
+        #since we know that edges always represents a valid star graph, we can just check which is common and elimiate time and space complexity - constant time and space
         if edges[0][0] in edges[1]:
             return edges[0][0]
         else:
