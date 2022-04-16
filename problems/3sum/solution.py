c89c9ae24809f94f7141fb45605356b1a3ef1559
Gowthami B <gowthamibhogireddy@gmail.com>
2022-04-16 04:08:45 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,25 +1,51 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        # i, j = 0,1
+        # n = len(nums)
+        # if n < 3:
+        #     return []
+        # nums.sort()
+        # res = set()
+        # for i in range(n - 2):
+        #     for j in range(i+1, n-1):
+        #         rem = -nums[i] - nums[j]
+        #         s, l = j+1, n-1
+        #         while(s <= l):
+        #             mid = (s+l)//2
+        #             if(nums[mid] == rem):
+        #                 res.add((nums[i],nums[j],nums[mid]))
+        #                 break
+        #             elif(nums[mid] < rem):
+        #                 s = mid + 1
+        #             else:
+        #                 l = mid - 1
+        # return [list(ele) for ele in res]
+        
+        n = len(nums)
         nums.sort()
-        for i in range(len(nums)):
+        res = []
+        if n < 3:
+            return []
+        for i in range(n):
             if nums[i] > 0:
                 break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
+            if i == 0 or nums[i-1] != nums[i]:
+                self.twosum(nums, i, res)
         return res
-
-    def twoSumII(self, nums: list[int], i: int, res: list[list[int]]):
-        lo, hi = i + 1, len(nums) - 1
-        while (lo < hi):
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
-                lo += 1
-            elif sum > 0:
-                hi -= 1
-            else:
-                res.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
+    def twosum(self,nums: List[int], i: int, res: List[List[int]]):
+            lo,hi = i+1, len(nums)-1
+            while(lo < hi):
+                sum = nums[i] +  nums[lo] +  nums[hi]
+                if sum == 0 :
+                    res.append([ nums[i], nums[lo], nums[hi]])
                     lo += 1
+                    hi -=1
+                    while lo < hi and nums[lo] == nums[lo - 1]:
+                        lo += 1
+                elif sum < 0:
+                    lo += 1
+                else:
+                    hi -= 1
+    
+                        
+                        
