cafd6f1fc5f13c7852a1b50eb3f7ff76dc7bb2dc
Gowthami B <gowthamibhogireddy@gmail.com>
2022-04-24 16:59:16 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- problems/valid_anagram/solution.py
+++ problems/valid_anagram/solution.py
@@ -1,20 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        window = {}
-        if len(s) != len(t):
-            return False
-        i = 0
-        while i < len(s):
-            if(s[i] in window):
-                window[s[i]] += 1
-            else:
-                window[s[i]] = 1
-            i+= 1
-        i = 0
-        while i < len(t):
-            if(t[i] not in window):
+        res = [0] * 26
+        for c in s:
+            res[ord(c) - ord('a')] += 1
+        for c in t:
+            res[ord(c) - ord('a')] -= 1
+        for v in res:
+            if v >0 or v < 0:
                 return False
-            else:
-                window[t[i]] -= 1
-            i+=1
-        return all(value == 0 for value in window.values())
+        return True
