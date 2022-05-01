51d3a2c75f73872115e63c530197fad45e50104d
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-05-01 10:08:39 -0400

    Time: 64 ms (35.53%), Space: 16.3 MB (57.53%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,48 @@
+from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        #One could traverse the tree either by Depth-First Search (DFS) strategy or Breadth-First Search (BFS) strategy Here we demonstrate a solution that is implemented with the DFS strategy (in order traversal, bcs we go to root, then left, then its children, pop back to root and go to right) and recursion.
+            # if root is None:
+            #     return 0
+            # left  = self.maxDepth(root.left)
+            # right = self.maxDepth(root.right)
+            # print(root,left,right)
+            # return max(left, right) + 1#plus 1 is for root
+            if root is None: 
+                return 0 
+            else: 
+                left_height = self.maxDepth(root.left) 
+                right_height = self.maxDepth(root.right) 
+                return max(left_height, right_height) + 1 
+        #BFS on a tree is level order traversal, BFS == queue
+        # if root is None:
+        #         return 0
+        # queue = deque([root])
+        # levels = 0
+        # while(queue):
+        #     for i in range(len(queue)): #we need to finish whole level, hence loop through all nodes in the level
+        #         node = queue.popleft()
+        #         print(node)
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #     levels += 1
+        # return levels
+        #iterative DFS - pre order traversal, root, left , right
+        # stack = [[root,1]]
+        # res = 0
+        # while(stack):
+        #     node, depth = stack.pop()
+        #     print(node, depth)
+        #     if node:
+        #         res = max(res, depth)
+        #         stack.append([node.right,depth+1])
+        #         stack.append([node.left,depth+1])
+        # return res
