451de02b1b3f492d5d443a4ec40104cd28a6d7fe
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-05-02 10:47:59 -0400

    Time: 161 ms (47.16%), Space: 14.1 MB (96.57%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -1,9 +1,26 @@
+from collections import OrderedDict
+from collections import Counter
 class Solution:
+    # def firstUniqChar(self, s: str) -> int:
+    #     charCount = OrderedDict()
+    #     for c in s:
+    #         if c in charCount:
+    #             charCount[c] += 1
+    #         else:
+    #             charCount[c] = 1
+    #     for k,v in charCount.items():
+    #         if v == 1:
+    #             return s.find(k)
+    #     return -1
+    
     def firstUniqChar(self, s: str) -> int:
-        count = collections.Counter(s)
-        # i = collections.Counter(1920120129102)
-        # print(i)
-        for i, val in enumerate(s):
-            if count[val] == 1:
-                return i
+        charCount = Counter(s) 
+        #Counter({'e': 3, 'l': 1, 't': 1, 'c': 1, 'o': 1, 'd': 1})
+# Counter({'e': 4, 'l': 2, 'o': 2, 'v': 1, 't': 1, 'c': 1, 'd': 1})
+# Counter({'a': 2, 'b': 2}) - first counts by highest frequency, then original order in string
+        print(charCount)
+        for idx,ch in enumerate(s):#we also get a need to keep a count of iterations.
+            if charCount[ch] == 1:
+                return idx
         return -1
+    
