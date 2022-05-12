05918c95f7d94ac9d660c103de8e60d1106c0717
Gowthami B <gowthamibhogireddy@gmail.com>
2022-05-12 12:55:15 +0000

    Sync LeetCode submission - Group Anagrams (python3)

--- problems/group_anagrams/solution.py
+++ problems/group_anagrams/solution.py
@@ -1,8 +1,15 @@
-import collections
 from collections import defaultdict
-class Solution(object):
-    def groupAnagrams(self, strs):
-        ans = collections.defaultdict(list)
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        wordsMap = defaultdict(list)
+        res = []
         for s in strs:
-            ans[tuple(sorted(s))].append(s)
-        return ans.values()
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            wordsMap[tuple(count)].append(s)
+            # wordsMap["".join(sorted(s))].append(s)
+        print(wordsMap)
+        for key,val in wordsMap.items():
+            res.append(val)
+        return res
