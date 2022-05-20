902b44a4d1fb3d040938ae4be8060c5aa7ae8205
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-05-20 12:42:31 -0400

    Time: 62 ms (55.28%), Space: 19.5 MB (13.31%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -1,13 +1,14 @@
 import heapq
 from collections import Counter
 class Solution:
-    def frequencySort(self, s: str) -> str:
+    def frequencySortNormal(self, s: str) -> str:
         charmap = Counter(s)
         print(charmap)
         # charmap = defaultdict(int)
         # for c in s:
         #     charmap[c] += 1
         res = ""
+            
         for char, freq in charmap.most_common():
             print(char, freq)
             res += freq * char
@@ -18,3 +19,19 @@ class Solution:
         #     freq, char = heapq.heappop(heap)
         #     res += abs(freq) * char
         return res
+    
+    def frequencySort(self, s: str) -> str:
+        charmap = Counter(s)
+        maxFreq = max(charmap.values())
+        res = ""
+        print(charmap)
+        buckets = [[] for _ in range(maxFreq+1)]
+        for char, freq in charmap.items():
+            buckets[freq].append(char)
+        print(buckets)
+        for i in range(len(buckets)-1,0,-1):
+            for c in buckets[i]:
+                res += i * c
+        return res
+                
+            
