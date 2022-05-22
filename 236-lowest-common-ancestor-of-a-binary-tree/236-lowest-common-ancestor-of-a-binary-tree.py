fd78a061e566803065a367c20b0fedeb7b7249e8
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-05-22 16:35:17 -0400

    Time: 85 ms (64.00%), Space: 28.1 MB (7.67%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -5,18 +5,36 @@
 #         self.left = None
 #         self.right = None
 
+"""
+brute force -
+say 6,5
+- traverse from root to p - store path in set - [3,5,6]
+- traverse from root to q - [3,5]
+last common one is the least common ancestor
+
+say 0,8
+- traverse from root to 0, will take us through - [3,5,6,2,7,4,1,0,8],we will also have all these nodes
+
+Optimal Solution -
+- travel from p to parent instead
+- set Parent for nodes
+- traverse through parents and mark as visited
+- traverse from q to parent, first node in visited is the lowest common
+"""
+
 class Solution:
+    def __init__(self):
+        self.visited = set()
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         self.setParent(root)
-        visited = set()
-        while p:
-            visited.add(p.val)
+        while p is not None:
+            self.visited.add(p.val)
             p = p.parent
-
         while q is not None:
-            if q.val in visited:
+            if q.val in self.visited:
                 return q
             q = q.parent
+        return None
     
     def setParent(self, root, par=None):
         if root is None:
