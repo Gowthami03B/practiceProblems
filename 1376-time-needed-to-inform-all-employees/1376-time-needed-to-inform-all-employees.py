b96b394a274e1f5a2d78448859fc5fbfbad26b56
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-05-28 19:01:11 -0400

    Time: 1500 ms (75.81%), Space: 42.8 MB (70.03%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,11 +1,10 @@
-from collections import defaultdict
 from collections import deque
 class Solution:
-    def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+    def numOfMinutesTest(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         graph = collections.defaultdict(list)
         for emp, mgr in enumerate(manager):
             if mgr != -1:
-                graph[mgr].append(emp)
+                graph[mgr].append(emp) #build adjacency list
         print(graph)
         def dfs(head, cur, maxtime):
             if not graph[head]:
@@ -13,7 +12,7 @@ class Solution:
             for emp in graph[head]:
                 maxtime = dfs(emp, cur+informTime[head], maxtime)
             return maxtime
-        return dfs(headID, 0, 0)
+        return dfs(headID, 0, 0) #start with head
     
     
 #         if len(manager) == 0 or (len(manager) == 1 and manager[0] == -1):
@@ -46,4 +45,24 @@ class Solution:
 #                         print(tempTime)      
 #             sumTime += tempTime
 #         return sumTime if sumTime !=0 else informTime[headID]
-    
+    def numOfMinutes(self, n, headID, manager, informTime):
+        
+        graph = collections.defaultdict(list)
+        for emp, mgr in enumerate(manager):
+            if mgr != -1:
+                graph[mgr].append(emp) #build adjacency list
+        print(graph)
+
+        # bfs 
+        queue = collections.deque()
+        queue.append((headID, 0)) #mgr, time taken to inform manager
+        totTime = 0
+
+        #for each emp, store the emp and time that was needed to inform that emp
+        while queue:
+            for i in range(len(queue)):
+                currmgr, currtime = queue.popleft()
+                totTime = max(totTime, currtime)
+                for emp in graph[currmgr]: 
+                    queue.append((emp, informTime[currmgr] + currtime))
+        return totTime
