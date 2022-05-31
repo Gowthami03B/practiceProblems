1b384759d68c6652b1ce9dda00fa954b422cc039
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-05-31 19:56:19 -0400

    Time: 56 ms (17.21%), Space: 13.9 MB (74.76%) - LeetHub

--- 100-same-tree/100-same-tree.py
+++ 100-same-tree/100-same-tree.py
@@ -7,14 +7,17 @@
 from collections import deque
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # if p is None or q is None:#if p or q is None, return False before recursion
+        #         return False
+        return self.dfs(p,q)  
+    
+    def dfs(self,p: Optional[TreeNode], q: Optional[TreeNode]):
             if p is None and q is None:
                 return True
-            if not p or not q:
+            elif p is None and q is not None: #either one is None, false
                 return False
-            if p.val == q.val:
-                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-            else:
+            elif q is None and p is not None:
                 return False
-
-                
-                
+            if p.val != q.val: #if values don't match false
+                return False
+            return self.dfs(p.left, q.left) and self.dfs(p.right, q.right) #need to combine the results from left and right, each returns a bool value
