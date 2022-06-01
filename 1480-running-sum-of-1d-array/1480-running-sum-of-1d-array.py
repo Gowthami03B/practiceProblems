c5ce6be997dfa5e84ffdee044901b82377c16844
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-05-31 21:11:18 -0400

    Time: 31 ms (99.48%), Space: 14.2 MB (27.03%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -1,6 +1,13 @@
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        """
+        extra space
         res = []
         for i in range(len(nums)):
             res.append(sum(nums[:i+1]))
         return res
+        """
+        #without extra space, you just have to add to the prev sum
+        for i in range(1,len(nums)):
+            nums[i] += nums[i-1]
+        return nums
