a7bdc7fb665660b8762445c74188744624b1020e
Gowthami B <gowthamibhogireddy@gmail.com>
2022-06-02 16:20:52 +0000

    Sync LeetCode submission - Meeting Rooms (python3)

--- problems/meeting_rooms/solution.py
+++ problems/meeting_rooms/solution.py
@@ -1,9 +1,7 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key=lambda x : x[0])
-        merged = []
-        for interval in intervals:
-            if not merged or merged[-1][1] <= interval[0] :
-                merged.append(interval)
-
-        return True if len(merged) == len(intervals) else False
+        intervals.sort()
+        for i in range(1,len(intervals)):#sort intervals and check if end of 1st interval < start of next interval
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
+        return True
