3dc04b384297bae1c333d0cf52707a28f1ee4243
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-02 12:18:07 -0400

    Time: 129 ms (25.78%), Space: 17.4 MB (37.48%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.py
+++ 252-meeting-rooms/252-meeting-rooms.py
@@ -1,9 +1,8 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort(key=lambda x : x[0])
-        merged = []
-        for interval in intervals:
-            if not merged or merged[-1][1] <= interval[0] :
-                merged.append(interval)
-
-        return True if len(merged) == len(intervals) else False
+        intervals.sort()
+        print(intervals)
+        for i in range(1,len(intervals)):
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
+        return True
