8c7e121f8018567c547b6a514b27d834546d0f22
Gowthami B <gowthamibhogireddy@gmail.com>
2022-06-02 21:54:10 +0000

    Sync LeetCode submission - Meeting Rooms II (python3)

--- problems/meeting_rooms_ii/solution.py
+++ problems/meeting_rooms_ii/solution.py
@@ -1,11 +1,14 @@
+from heapq import heappush, heappop
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key=lambda x:x[0])
-        freerooms = []
-        heapq.heappush(freerooms, intervals[0][1])
-        for i in intervals[1:]:
-            if freerooms[0] <= i[0]:
-                heapq.heappop(freerooms)
-            heapq.heappush(freerooms, i[1])
-
-        return len(freerooms)
+        intervals.sort()
+        if len(intervals) == 0:
+            return 0
+        roomsNeeded = []
+        heappush(roomsNeeded, intervals[0][1]) #we push the end time of first meeting
+        for interval in intervals[1:]: #for all subsequent meetings
+            if interval[0] >= roomsNeeded[0]: #if next start interval is >= first element on heap, means the new interval can be scheduled in the same room, [4,8][10,12]
+                heappop(roomsNeeded) #hence we pop
+            heappush(roomsNeeded, interval[1]) #we only pop when above condition is met, else we push the new interval with it's end time
+        return len(roomsNeeded)
+        
