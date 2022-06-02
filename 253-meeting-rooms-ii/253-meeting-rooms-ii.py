23d688ea2149d5c6d88452a7a5fce26b4787f74e
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-02 17:54:14 -0400

    Time: 135 ms (24.05%), Space: 17.4 MB (82.78%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -9,6 +9,6 @@ class Solution:
         for interval in intervals[1:]: #for all subsequent meetings
             if interval[0] >= roomsNeeded[0]: #if next start interval is >= first element on heap, means the new interval can be scheduled in the same room, [4,8][10,12]
                 heappop(roomsNeeded) #hence we pop
-            heappush(roomsNeeded, interval[1]) #else we push the new interval with it's end time
+            heappush(roomsNeeded, interval[1]) #we only pop when above condition is met, else we push the new interval with it's end time
         return len(roomsNeeded)
         
