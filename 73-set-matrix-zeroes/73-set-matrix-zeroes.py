a5eee86d62a6d25077f172c60cf6777f428272da
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-03 09:54:01 -0400

    Time: 184 ms (43.38%), Space: 14.9 MB (16.20%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -5,19 +5,21 @@ class Solution:
         for i in range(rows):
             if matrix[i][0] == 0:#check if first col needs to be set to 0's
                 isCol = True
-            for j in range(1,cols):
+            for j in range(1,cols):#leaving first column, loop through others and mark the start of row and col as 0's
                 if matrix[i][j] == 0:
                     matrix[i][0] = 0
                     matrix[0][j] = 0
                     
+        #leaving 1st row and col, check when start rows and cols are 0, set intermediary elements also to 0   
         for i in range(1,rows):
             for j in range(1,cols):
-                if not matrix[i][0] or not matrix[0][j]:
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
-                    
+        #if first row is 0, then increment cols and set to 0      
         if matrix[0][0] == 0:
             for j in range(cols):
                 matrix[0][j] = 0
+         #if first col is 0, then increment rows and set to 0
         if isCol:
             for i in range(rows):
                 matrix[i][0] = 0
