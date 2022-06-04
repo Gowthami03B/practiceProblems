503d0038f726ae96e772f618b815ae6aac7b42dc
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-04 11:53:58 -0400

    Time: 661 ms (63.67%), Space: 35.8 MB (83.04%) - LeetHub

--- 295-find-median-from-data-stream/295-find-median-from-data-stream.py
+++ 295-find-median-from-data-stream/295-find-median-from-data-stream.py
@@ -32,3 +32,55 @@ class MedianFinder:
 # obj = MedianFinder()
 # obj.addNum(num)
 # param_2 = obj.findMedian()
+
+"""
+If all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+1) create hashmap for 1 to 100
+2) count values for each number
+3) if total values is odd, then median value -findkth(size//2+1), size//2+1 is the mid value in an odd number array, we check when the count of values is >= size//2 + 1 and return it
+4) if total values is even, (self.findkth(size // 2 + 1) + self.findkth(size // 2))/2
+
+Time complexity - addNum - O(1), findMedian - O(100)
+Space - O(100)
+
+If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
+1) create hashmap for 1 to 101 and for all values >100, count under 101
+same as above
+Time complexity - addNum - O(1), findMedian - O(100)
+Space - O(101)
+
+class MedianFinder:
+    def __init__(self):
+        self.counter = {i: 0 for i in range(1, 102)} #dictionary
+        
+    def addNum(self, num):
+        if num <= 100:
+            self.counter[num] += 1
+        else:
+            self.counter[101] += 1#Bucket sorting. Key=101 is to store the outliner values that greater than 100.
+        print(self.counter)
+            
+    def findMedian(self):
+        size = sum(self.counter.values()) #total number of values
+        if size % 2 == 1: #odd count middle value
+            return self.findkth(size // 2 + 1)
+        else:
+            return 0.5 * (self.findkth(size // 2 + 1) + self.findkth(size // 2))
+        
+    def findkth(self, k):
+        count = 0
+        for i in range(1, 102):#for i in range(1,100)
+            count += self.counter[i] #count the total number of values
+            if count >= k: #if count >= mid value, return i (the median value)
+                return i
+
+obj = MedianFinder()
+obj.addNum(1)
+obj.addNum(2)
+obj.addNum(5)
+obj.addNum(7)
+obj.addNum(10)
+obj.addNum(20)
+param_2 = obj.findMedian()
+print(param_2)
+    """
