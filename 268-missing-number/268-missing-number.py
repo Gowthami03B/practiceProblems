45ecf31d57e6d63061b9c5d5ce7db17f517532de
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-04 12:16:36 -0400

    Time: 321 ms (15.07%), Space: 15.8 MB (6.41%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -6,14 +6,21 @@ class Solution:
             if n not in nums: #in operation in list is O(n)
                 return n
             
-    def missingNumber(self, nums: List[int]) -> int:
+    def missingNumberSorting(self, nums: List[int]) -> int:
         nums.sort()
         if nums[0] != 0:
             return 0
         if nums[-1] != len(nums):
             return len(nums)
-        #O(n log n) time complexity - sorting
+        #O(n log n) time complexity - sorting far better
         for i in range(1, len(nums)):
             expected = nums[i-1] + 1
             if nums[i] != expected:
                 return expected
+            
+    #O(n) time complexity
+    def missingNumber(self, nums: List[int]) -> int:
+        num_set = set(nums)
+        for num in range(len(nums) + 1):
+            if num not in num_set: #O(1)- in operation on set is O(1)
+                return num
