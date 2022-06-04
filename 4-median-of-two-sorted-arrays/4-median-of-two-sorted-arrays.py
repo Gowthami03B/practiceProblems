ab7e58838152eb3b77e104543123018ce6defb5d
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-04 16:31:21 -0400

    Time: 123 ms (54.85%), Space: 14.2 MB (24.74%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,7 +1,50 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        """
+        #an array of size 13, can be partitioned as 2 around the middle element
+        #so basically we need to know the bounds of our left partition.
+        A = [1,2,2,4]B =  [2,3,3,5,7,8,9]
+        [1,2,2,2,3,3,4,5,7,8,9] #last 3 is the mid element
+        [1,2] [2,3,3] 5 <= 2; False
+        [1,2,2] [2,3] 3<= 4 and 4 <= 5 - mid is min(3,4)
+        
+        A = [1,2,2,3] B = [2,4,5,7,8,9]
+        [1,2] [2,4,5] (left partition 5) but 5<=2 false expand A's mid
+        [1,2,2] [2,4] but 5<=3 false expand A's mid
+        [1,2,2,3] [2] 4 <infinity 
+        mid is the max of left + min of right /2 i.e 3+4/2
+        [1,2,2,2,3,4,5,7,8,9] #max of right partition 
+        """
+        A,B = nums1, nums2
+        m,n = len(nums1),len(nums2)
+        totLen = m + n
+        #binary search on smaller array
+        if n < m:
+            A,B = B,A
+        l , r = 0, len(A) - 1
+        while True:
+            mid = (l + r)//2
+            j = (totLen//2) - mid - 2 #say total 13, 6 elements 1 half, mid of A is 1 (means 0 and 1 indices, 2 elements), 4 elements in B from 0,1,2,3 hence 6 - 1 - 2
+            Aleft = A[mid] if mid >= 0 else float('-inf')
+            Bleft = B[j] if j >= 0 else float('-inf')
+            Aright = A[mid + 1] if (mid + 1) < len(A) else float('inf')
+            Bright = B[j+1] if (j + 1) < len(B) else float('inf')
+            
+            if Aleft <= Bright and Bleft <= Aright:
+                if totLen % 2:
+                    return min(Aright, Bright)
+                else:
+                    return (max(Aleft, Bleft) + min(Aright , Bright))/2
+            elif Bleft > Aright:
+                l = mid + 1
+            else:
+                r = mid - 1
+    
+    #with space, use merge sort technique
+    def findMedianSortedArraysMergeSort(self, nums1: List[int], nums2: List[int]) -> float:
         nums = []
         i,j = 0,0
+        #check if nums1 < nums2, then update nums accordingly
         while(i < len(nums1) and j < len(nums2)):
             if nums1[i] < nums2[j]:
                 nums.append(nums1[i])
@@ -9,7 +52,7 @@ class Solution:
             else:
                 nums.append(nums2[j])
                 j += 1
-
+        #fill the remaining elements from both arrays in nums
         while(i < len(nums1)):
             nums.append(nums1[i])
             i+= 1
@@ -20,6 +63,7 @@ class Solution:
 
         print(nums,i,j)
         mergedArrayLen = len(nums)
+        #len / 2 is the median of the odd length array and if it is even, average of len / 2th and len / 2 – 1
         if mergedArrayLen % 2 ==1:
             return nums[mergedArrayLen//2]
         else:
