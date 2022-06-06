9e823ce6c5906307581bd5e20a20c756aa7a03af
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-05 20:47:13 -0400

    Time: 348 ms (95.02%), Space: 14.3 MB (31.90%) - LeetHub

--- 485-max-consecutive-ones/485-max-consecutive-ones.py
+++ 485-max-consecutive-ones/485-max-consecutive-ones.py
@@ -1,9 +1,17 @@
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        start = 0
+        count = 0
         maxLen = 0
+        # for i in range(len(nums)):
+        #     if nums[i] == 0:
+        #         start = i + 1 
+        #     maxLen = max(maxLen, i - start + 1)
+        # return maxLen
+    
         for i in range(len(nums)):
-            if nums[i] == 0:
-                start = i + 1 
-            maxLen = max(maxLen, i - start + 1)
-        return maxLen
+            if nums[i]:
+                count += 1
+            else:
+                maxLen = max(maxLen, count)
+                count = 0
+        return max(maxLen,count)
