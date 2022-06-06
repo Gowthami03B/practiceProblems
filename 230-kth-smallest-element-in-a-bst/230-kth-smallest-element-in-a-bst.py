65a2cb292755999f1cc791fa2fb20183058dccb6
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-06 17:18:04 -0400

    Time: 128 ms (5.03%), Space: 18 MB (41.99%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -0,0 +1,48 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallestRecursive(self, root: Optional[TreeNode], k: int) -> int:
+        def inorder(root):
+            #at every node we return a list, [left] + [root] + [right]
+            return inorder(root.left) + [root.val] + inorder(root.right) if root else []
+        return inorder(root)
+    
+    def kthSmallestRecursive1(self, root: Optional[TreeNode], k: int) -> int:
+        res = []
+        def inorder(root):
+            if not root:
+                return []
+            left = inorder(root.left)
+            res.append(root.val)
+            right = inorder(root.right)
+            return res
+        return inorder(root)[k-1]
+    
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        res, stack = [], []
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                root = node.right
+        return res[k-1]
+    
+    def kthSmallestDesc(self, root: Optional[TreeNode], k: int) -> int:
+        res, stack = [], []
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.right
+            else:
+                node = stack.pop()
+                res.append(node.val)
+                root = node.left
+        print(res)
+            
