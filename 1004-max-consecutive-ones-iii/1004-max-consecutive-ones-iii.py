516a60e4b8bf63563bd6a85a746f450bfc203dde
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-05 20:32:01 -0400

    Time: 618 ms (89.40%), Space: 14.7 MB (19.53%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,13 +3,13 @@ class Solution:
         start, i = 0,0 #start=window start
         maxLen = 0
         for i in range(len(nums)):
-            if nums[i] == 0: #if encounter 0, increment count
+            if nums[i] == 0: #if encounter 0, decrement K
                 k -= 1
             while(k < 0): #when k becomes less than 0, we need to slide our window
-                if nums[start] == 0:
+                if nums[start] == 0:#if first element is 0, adjust k, if we slide the window and disregard this 0, need to increment k
                     k += 1
-                start += 1
-            maxLen = max(maxLen, i - start + 1)
+                start += 1#slide window by 1 position
+            maxLen = max(maxLen, i - start + 1) #find maxLength
         return maxLen
         
     def longestOnesMyApproach(self, nums: List[int], k: int) -> int:
