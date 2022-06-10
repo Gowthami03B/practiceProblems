853bd9901376f74f77c589264dcb764b7f5e3d7a
Gowthami B <gowthamibhogireddy@gmail.com>
2022-06-10 21:09:36 +0000

    Sync LeetCode submission - Search Insert Position (python3)

--- problems/search_insert_position/solution.py
+++ problems/search_insert_position/solution.py
@@ -1,13 +1,31 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        left  = 0
-        right = len(nums) - 1
-        while right >= left:
-            mid = (left + right) //2
-            if target > nums[mid]:
-                left = mid + 1
-            elif target < nums[mid]:
-                right = mid - 1
-            else:
+    def searchInsertBruteForce(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while(l <= r):
+            mid = (l+r)//2
+            if nums[mid] == target:
                 return mid
-        return left
+            elif nums[mid] < target and mid == len(nums) - 1:
+                return mid + 1
+            elif nums[mid] > target and mid == 0:
+                return mid
+            elif nums[mid] > target and nums[mid - 1] < target:
+                return mid
+            elif nums[mid] < target and nums[mid + 1] > target:
+                return mid + 1
+            elif nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+    
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while(l <= r):
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+        return l
