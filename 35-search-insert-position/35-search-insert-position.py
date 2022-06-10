2024e0dcff4021acf3380eba8a9acc20a19c74fa
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-10 17:09:39 -0400

    Time: 65 ms (55.73%), Space: 14.8 MB (6.09%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -1,5 +1,5 @@
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsertBruteForce(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) - 1
         while(l <= r):
             mid = (l+r)//2
@@ -17,3 +17,15 @@ class Solution:
                 l = mid + 1
             elif nums[mid] > target:
                 r = mid - 1
+    
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+        while(l <= r):
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
+        return l
