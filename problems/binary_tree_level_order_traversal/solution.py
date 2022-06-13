8b12c692e12e847641b690291206dbe6edafa295
Gowthami B <gowthamibhogireddy@gmail.com>
2022-06-13 14:56:11 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (python3)

--- problems/binary_tree_level_order_traversal/solution.py
+++ problems/binary_tree_level_order_traversal/solution.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    def levelOrder1(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
         res = []
@@ -22,3 +22,18 @@ class Solution:
                 res1.append(child.val)
             res.append(res1)
         return res
+    
+    #level order traversal with stack
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        stack, res = [(root,0)], collections.defaultdict(list)
+        while stack:
+            root , height = stack.pop()
+            if root:
+                if root.right:
+                    stack.append((root.right, height + 1))
+                if root.left:
+                    stack.append((root.left, height + 1))
+                res[height].append(root.val)
+        return list(res.values())
