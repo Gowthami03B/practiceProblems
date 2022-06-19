5d3cfe34a260e4706a5bbf485a0a75382a54ca7e
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-19 12:43:08 -0400

    Time: 417 ms (13.25%), Space: 20.6 MB (86.00%) - LeetHub

--- 1236-web-crawler/1236-web-crawler.py
+++ 1236-web-crawler/1236-web-crawler.py
@@ -8,18 +8,21 @@
 #        :type url: str
 #        :rtype List[str]
 #        """
-import re
 class Solution:
     def crawl(self, startUrl: str, htmlParser: 'HtmlParser') -> List[str]:
-        hostname = startUrl.split('/')[2]
+        hostname = startUrl.split('/')[2] #splits the string at /
+        """
+        ['http:', '', 'news.yahoo.com', 'news', 'topics', ''] news.yahoo.com
+['http:', '', 'news.google.com'] news.google.com
+        """
         print(startUrl.split('/'), hostname)
-        queue = collections.deque([startUrl])
-        visited = set([startUrl])
+        queue = collections.deque([startUrl]) #start from startUrl
+        visited = set([startUrl]) #add it to visited
         while queue:
             surl = queue.popleft()
-            urls = htmlParser.getUrls(surl)
+            urls = htmlParser.getUrls(surl)#get the Urls
             for url in urls:
-                if url.split('/')[2] == hostname and url not in visited:
+                if hostname in url and url not in visited:#if its the same host and not in visited
                     queue.append(url)
                     visited.add(url)
         return list(visited)
