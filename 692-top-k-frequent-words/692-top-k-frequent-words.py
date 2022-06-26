103751444470cc9612e04fba06a78f64eba9a825
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-26 10:17:38 -0400

    Time: 126 ms (6.28%), Space: 14.1 MB (5.74%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.py
+++ 692-top-k-frequent-words/692-top-k-frequent-words.py
@@ -6,11 +6,24 @@ class Solution:
         for word in words:
             wordMap[word] += 1
         maxheap = []
+        #heap takes care of sorting
         for word, freq in wordMap.items():
             heappush(maxheap, (-freq, word))
-        
+        #list comprehensions
         return [heappop(maxheap)[1] for _ in range(k)]
         
         
-        
+    def topKFrequentMethod2(self, words: List[str], k: int) -> List[str]:
+        wordMap = Counter(words)
+        output = [x[0] for x in sorted(wordMap.items(), key=lambda kv:(-kv[1],kv[0]))[:k]]#gives a list sorted by words in frequency first and lexicographical next, selecting first k words
+        print(output)
+        heap, res = [],[]
+        for word, freq in wordMap.items():
+            heappush(heap, (-freq,word)) #constructing a max heap, heap also takes care of lexicographical order
+        print(heap)
+        for _ in range(k):
+            freq, word = heappop(heap) #pops the smallest element first or smallest lexicographical word
+            res.append(word)
+            
+        return res
         
