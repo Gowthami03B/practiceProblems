ad4a9e01d9d69ca7137f0bcc4db21c0ac739aea6
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-26 09:56:54 -0400

    Time: 62 ms (19.48%), Space: 13.9 MB (68.13%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.py
@@ -1,6 +1,14 @@
 class Solution:
-    def checkValidString(self, s: str) -> bool:
+    """
+    1- There are more open parenthesis but we have enough '*' so we can balance the parenthesis with ')'
+2- There are more close parenthesis but we have enough '*' so we can balance the parenthesis with '('
+3- There are as many '(' than ')' so all parenthesis are balanced, we can ignore the extra '*'
+    """
+    def checkValidString1(self, s: str) -> bool:
         leftBal, rightBal = 0,0
+        #left to right and check count of all (*
+        #here we consider * as open bracket
+        #if leftBal goes neg then brackets are not equal
         for val in s:
             if val in "(*":
                 leftBal += 1
@@ -10,6 +18,9 @@ class Solution:
             if leftBal < 0:
                 return False
     
+        #right to left and check count of all )*
+        #here we consider * as close bracket
+        #if rightBal goes neg then brackets are not equal
         for val in reversed(s):
             if val in ")*":
                 rightBal += 1
@@ -19,4 +30,14 @@ class Solution:
             if rightBal < 0:
                 return False
         
-        return True
+        return True #if both bal is 0 or left == right then opencnt == closecnt and TRUE
+    
+    def checkValidString(self, s: str) -> bool:
+        lo = hi = 0
+        for c in s:
+            lo += 1 if c == '(' else -1
+            hi += 1 if c != ')' else -1
+            if hi < 0: break
+            lo = max(lo, 0)
+
+        return lo == 0
