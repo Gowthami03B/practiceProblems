c412a7643ce72797b069cf90eda85554277a4d96
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-06-27 10:44:48 -0400

    Time: 66 ms (62.08%), Space: 13.9 MB (30.40%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,5 +1,5 @@
 class Solution:
-    def romanToInt(self, s: str) -> int:
+    def romanToInt1(self, s: str) -> int:
         mapping = {'I':1,'V' :5, 'X' :10,'L' :50, 'C':100,'D':500,'M':1000}
         i = 0
         sum = 0
@@ -12,3 +12,17 @@ class Solution:
                 i+=1
         return sum
             
+    def romanToInt(self, s: str) -> int:
+        romanEnum = {
+            "I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D": 500, "M" : 1000}
+
+        m = len(s) - 1
+        sum = 0
+        sum += romanEnum[s[m]]
+        m-=1
+        for i in range(m,-1,-1):
+            if romanEnum[s[i]] < romanEnum[s[i+1]]:
+                sum -= romanEnum[s[i]]
+            else:
+                sum += romanEnum[s[i]]
+        return sum
