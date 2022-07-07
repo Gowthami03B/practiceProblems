a62feb93175cb67464935217ff83d213e971554a
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-07-07 16:38:39 -0400

    Time: 45 ms (99.69%), Space: 14 MB (40.33%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.py
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -1,6 +1,6 @@
 import re
 class Solution:
-    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+    def subdomainVisitsComplex(self, cpdomains: List[str]) -> List[str]:
         #extract digits for count
         #find the first dot and any other dots
         #store domain name as key and append to count in a map
@@ -16,6 +16,18 @@ class Solution:
                 domain_count[".".join(split_domains[i:n+1])] += int(count)
             
         print(domain_count)
+        res = []
+        for key, val in domain_count.items():
+            res.append(str(val) + " " + key)
+        return res
+    
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        domain_count = defaultdict(int)
+        for domain in cpdomains:
+            domains = domain.split(" ")
+            tpdomains = domains[1].split(".")
+            for i in range(len(tpdomains)):
+                domain_count[".".join(tpdomains[i:])] += int(domains[0])
         res = []
         for key, val in domain_count.items():
             res.append(str(val) + " " + key)
