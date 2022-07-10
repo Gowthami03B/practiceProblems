9e78d80baabf407308e59ead13a67666b27a4f5d
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-07-10 10:11:49 -0400

    Time: 97 ms (62.78%), Space: 15.1 MB (50.41%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
+        val_map = defaultdict(int)
         for i in range(len(nums)):
-            rem = target - nums[i]
-            if rem in map:
-                return [i, map[rem]]
-            map[nums[i]] = i
+            complement = target - nums[i]
+            if complement in val_map:
+                return [i, val_map[complement]]
+            val_map[nums[i]] = i
+        return []
