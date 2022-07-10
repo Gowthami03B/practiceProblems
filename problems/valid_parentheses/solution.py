1d988ccafe985d4e10050bfcdec38f0316338976
Gowthami B <gowthamibhogireddy@gmail.com>
2022-07-10 21:56:55 +0000

    Sync LeetCode submission - Valid Parentheses (python3)

--- problems/valid_parentheses/solution.py
+++ problems/valid_parentheses/solution.py
@@ -1,13 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        if len(s) == 1:
+            return False
+        parenthesis_map = {"}": "{", "]":"[",")":"("}#map closing to open
         stack = []
-        window = {'}' : '{', ']' : '[',')' : '('}
         for c in s:
-            if c in window:
-                topElement = stack.pop() if stack else '#'
-                if window[c] != topElement:
+            if c in parenthesis_map:#for all closing brackets
+                topEle = stack.pop() if stack else '#'#get top element if stack else return a dummy element so we can return False even if one pairing doesn't match
+                if parenthesis_map[c] != topEle:#if mapping doesn't exist in map
                     return False
             else:
-                stack.append(c)
-        if not stack:
-            return True
+                stack.append(c)#for all opening brackets
+        return True if not stack else False
+                    
