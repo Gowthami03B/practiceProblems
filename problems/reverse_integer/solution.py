9decacc7eb94b638524d4a9c4be209c8d7d86f4f
Gowthami B <gowthamibhogireddy@gmail.com>
2022-07-10 22:42:15 +0000

    Sync LeetCode submission - Reverse Integer (python3)

---
+++ problems/reverse_integer/solution.py
@@ -0,0 +1,39 @@
+class Solution:
+    #convert to string
+    def reverse1(self, x: int) -> int:
+        if x == 0:
+            return 0
+        elif x > 0:
+            reversed_int = int(str(x)[::-1])#reverse the number check the limits
+            if -2**31 <= reversed_int <= 2**31 - 1:
+                return reversed_int
+            else:
+                return 0
+        else:
+            reversed_int = -int(str(-x)[::-1])
+            if -2**31 <= reversed_int <= 2**31 - 1:
+                return reversed_int
+            else:
+                return 0
+            
+    def reverse(self, x: int) -> int:
+        if x == 0:
+            return 0
+        neg = False
+        if x < 0:
+            neg = True
+        x = abs(x)
+        reversed_int = 0
+        while x > 0:
+            reminder = x % 10
+            x = x//10
+            reversed_int = reversed_int * 10 + reminder #construct the reverse number
+        if neg:
+            reversed_int = -reversed_int
+        if -2**31 <= reversed_int <= 2**31 - 1:#check the boundaries
+            return reversed_int
+        else:
+            return 0
+            
+            
+            
