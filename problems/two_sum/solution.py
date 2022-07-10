39b2c7679be6ff29d60dacb63511acdb8dd3bc1e
Gowthami B <gowthamibhogireddy@gmail.com>
2022-07-10 14:11:43 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,13 +1,9 @@
-from collections import defaultdict
-class Solution(object):
-    def twoSum(self, nums, target):
-        n = len(nums)
-        window = defaultdict(int)
-        for i in range(n):
-            if(nums[i] in window):
-                return [i, window[nums[i]]]
-            else:
-                window[target - nums[i]] = i
-        return s
-
-        
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        val_map = defaultdict(int)
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in val_map:
+                return [i, val_map[complement]]
+            val_map[nums[i]] = i
+        return []
