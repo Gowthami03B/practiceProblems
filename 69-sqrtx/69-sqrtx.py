aa86874a589fac094a064a6e4efe1edabd96ad62
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-07-11 17:08:04 -0400

    Time: 54 ms (65.03%), Space: 14 MB (11.16%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,5 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        #since we only have to return the integer part, and since the square root is < x/2, we only search in that space, find a number and see if n*n < x or > x 
         if x == 0 or x == 1:
             return x
         start , end = 1, x//2
