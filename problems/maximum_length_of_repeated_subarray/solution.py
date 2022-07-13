678fe4dbfed37a1a56d6d1233dd522dcdac9efc9
Gowthami B <gowthamibhogireddy@gmail.com>
2022-07-13 19:41:18 +0000

    Sync LeetCode submission - Maximum Length of Repeated Subarray (python3)

--- problems/maximum_length_of_repeated_subarray/solution.py
+++ problems/maximum_length_of_repeated_subarray/solution.py
@@ -1,13 +1,23 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
-        N,M = len(nums1), len(nums2)
-        dp = [[0 for _ in range(M+1)] for _ in range(N+1)]
-        output = 0
-        for i in range(1,N+1):
-            for j in range(1, M+1):
-                if nums1[i-1] == nums2[j-1]:
-                    dp[i][j] = 1+dp[i-1][j-1]
-                output = max(output, dp[i][j])
-                
-        return output
-                    
+        #longest subarray would be prev len + 1 which is [i-1] and [j-1]
+        #since starting with 1, you need to have +1 in array
+        #similar to longest common subsequence
+        dp = [[0 for _ in range(len(nums2) + 1)] for _ in range(len(nums1) + 1)]
+        max_len = 0
+        # for i in range(1,len(nums1) + 1):#since starting with 1, you need to have +1 in array
+        #     for j in range(1,len(nums2) + 1):
+        #         if nums1[i-1] == nums2[j-1]:#compare all values in nums1 with nums2
+        #             dp[i][j] = dp[i-1][j-1] + 1
+        #             max_len = max(max_len,dp[i][j])
+        # print(dp)
+        # return max_len
+    
+        for i in reversed(range(len(nums1))):
+            for j in reversed(range(len(nums2))):
+                if nums1[i] == nums2[j]:#compare all values in nums1 with nums2
+                    dp[i][j] = dp[i+1][j+1] + 1
+                    max_len = max(max_len,dp[i][j])
+        return max_len
+    
+    
