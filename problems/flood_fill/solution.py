97641336448db5dfbea134b0fd8992d1f30b9a1a
Gowthami B <gowthamibhogireddy@gmail.com>
2022-07-13 14:19:07 +0000

    Sync LeetCode submission - Flood Fill (python3)

--- problems/flood_fill/solution.py
+++ problems/flood_fill/solution.py
@@ -1,19 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        if image == None:
-            return 
-        oldColor = image[sr][sc]
-        height = len(image)
-        width = len(image[1])
-
-        def dfs(sr,sc):
-            if 0 <= sr < height and 0 <= sc < width and image[sr][sc] == oldColor and image[sr][sc] != newColor:
-                image[sr][sc] = newColor
-                dfs(sr+1, sc)
-                dfs(sr-1,sc)
-                dfs(sr, sc+1)
-                dfs(sr, sc-1)
-            
-        dfs(sr,sc)
+        rows = len(image)
+        cols = len(image[0])
+        curr_color = image[sr][sc]
+        queue = deque()
+        visited = set()
+        dir = [[1,0],[0,1],[-1,0],[0,-1]]
+        for i in range(rows):
+            for j in range(cols):
+                if i == sr and j == sc:
+                    image[i][j] = newColor
+                    queue.append((i,j))
+                    visited.add((i,j))
+        while(queue):
+            r,c = queue.popleft()
+            for dr,dc in dir:
+                nr, nc = r + dr, c + dc
+                if nr in range(rows) and nc in range(cols) and image[nr][nc] == curr_color and (nr,nc) not in visited:
+                    image[nr][nc] = newColor
+                    queue.append((nr,nc))
+                    visited.add((nr,nc))
         return image
-        
+                    
