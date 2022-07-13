a78818bf697b8e2f5d3049e8e566a7a3d84b9179
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-07-13 11:06:11 -0400

    Time: 91 ms (48.98%), Space: 16.6 MB (11.17%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
@@ -5,14 +5,14 @@ class Solution:
         vowels = set(['a','e','i','o','u','A','E','I','O','U'])
         i,j = 0, len(s) - 1
         while(i < j):
-            if s[i] not in vowels:
+            if s[i] not in vowels:#if not move each pointer
                 i += 1
                 continue
             if s[j] not in vowels:
                 j -= 1
                 continue
                 
-            s[i],s[j] = s[j], s[i]
+            s[i],s[j] = s[j], s[i]#SWAP IF both are vowels
             i+=1
             j-=1
         return "".join(s)
