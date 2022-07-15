619441d58801f0150e7550cd65d7909f44f15126
Gowthami B <gowthamibhogireddy@gmail.com>
2022-07-15 02:56:17 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (python3)

--- problems/binary_tree_inorder_traversal/solution.py
+++ problems/binary_tree_inorder_traversal/solution.py
@@ -5,23 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # res = []
-        # def dfs(root: Optional[TreeNode]):
-        #     if root:
-        #         dfs(root.left) #dfs on root to print lowest leaf left node
-        #         res.append(root.val)#print it's root
-        #         dfs(root.right)#print right of lowest right node
-        #         return res
-        # return dfs(root)
+    def inorderTraversalRecursive(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(root):
+            nonlocal res
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
+        return res
     
-        res, stack = [], []
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        stack = []
+        res = []
         while stack or root:
             if root:
-                stack.append(root) #adding entire tree first, then whole of left subtree until the left leaf node
+                stack.append(root)
                 root = root.left
             else:
-                node = stack.pop()#pop the left most first 
-                res.append(node.val) #add it to res
-                root = node.right #leaf node won't have right, hence pops root next, adds root value to response, then right leaf of last root node
+                node = stack.pop()
+                res.append(node.val)
+                root = node.right
         return res
