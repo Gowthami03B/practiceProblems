965562e7ed8cbab5f40ae5ed5d91161000586da4
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-07-14 22:48:05 -0400

    Time: 35 ms (84.38%), Space: 13.9 MB (12.74%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -6,22 +6,14 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # res = []
-        # def dfs(root: Optional[TreeNode]):
-        #     if root:
-        #         dfs(root.left) #dfs on root to print lowest leaf left node
-        #         res.append(root.val)#print it's root
-        #         dfs(root.right)#print right of lowest right node
-        #         return res
-        # return dfs(root)
-    
-        res, stack = [], []
-        while stack or root:
-            if root:
-                stack.append(root) #adding entire tree first, then whole of left subtree until the left leaf node
-                root = root.left
-            else:
-                node = stack.pop()#pop the left most first 
-                res.append(node.val) #add it to res
-                root = node.right #leaf node won't have right, hence pops root next, adds root value to response, then right leaf of last root node
+        res = []
+        def dfs(root):
+            nonlocal res
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        dfs(root)
         return res
+            
