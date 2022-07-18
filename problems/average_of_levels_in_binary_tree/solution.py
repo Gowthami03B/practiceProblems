b979dba27e1914b6cfa1a88075c03cdf31e2df83
Gowthami B <gowthamibhogireddy@gmail.com>
2022-07-18 23:21:12 +0000

    Sync LeetCode submission - Average of Levels in Binary Tree (python3)

--- problems/average_of_levels_in_binary_tree/solution.py
+++ problems/average_of_levels_in_binary_tree/solution.py
@@ -6,23 +6,36 @@
 #         self.right = right
 from collections import deque
 class Solution:
+    def averageOfLevels1(self, root: Optional[TreeNode]) -> List[float]:
+        if root:
+            queue = deque([root])
+        res = []
+        while queue:
+            k, avg = len(queue), 0
+            for _ in range(k):
+                children = queue.popleft()
+                avg += children.val
+                if children.left:
+                    queue.append(children.left)
+                if children.right:
+                    queue.append(children.right)
+            res.append(avg/k)
+        return res
+        
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-            res = []
-            if root:
-                queue = deque([root])
-            while queue:
-                sum, k = 0,len(queue)
-                for _ in range(k):
-                    node = queue.popleft()
-                    sum += node.val
-                    if node.left:
-                        queue.append(node.left)
-                    if node.right:
-                        queue.append(node.right)
-                res.append(sum/k)
-            return res
-
-    
+        level_sum = defaultdict(int)
+        level_count = defaultdict(int)
+        def dfs(root, level=0):
+            if not root:
+                return
+            level_count[level] += 1
+            level_sum[level] += root.val
+            dfs(root.left, level +1)
+            dfs(root.right, level +1)
+        
+        dfs(root)
+        # print(level_sum, level_count)
+        return [level_sum[i]/level_count[i] for i in range(len(level_sum))] 
         
                 
         
