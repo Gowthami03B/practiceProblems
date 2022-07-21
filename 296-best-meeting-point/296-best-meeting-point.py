70dcbf7187d8171883488e99fb0a4316de4822cb
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-07-20 22:37:42 -0400

    Time: 146 ms (72.75%), Space: 16 MB (19.80%) - LeetHub

--- 296-best-meeting-point/296-best-meeting-point.py
+++ 296-best-meeting-point/296-best-meeting-point.py
@@ -1,5 +1,9 @@
 class Solution:
     def minTotalDistance(self, grid: List[List[int]]) -> int:
+        #in 1d, meeting point between points is the mid point
+        #1-1-0-0-1-1, 5/2 = 2, i.e first 0
+        #1-1-0-0-1
+        #hence we collect the xcoor, ycoord
         x_coords, y_coords = [],[]
         rows = len(grid)
         cols = len(grid[0])
@@ -9,8 +13,9 @@ class Solution:
                     x_coords.append(i)
                     y_coords.append(j)
                 
-        x_coords.sort()
+        x_coords.sort()#sort them
         y_coords.sort()
-        mid_x = x_coords[len(x_coords)//2]
+        mid_x = x_coords[len(x_coords)//2]#find the mid value of x and y
         mid_y = y_coords[len(y_coords)//2]
+        #sum of distances from start to mid for both x and y list
         return sum([abs(mid_x - x_coord) for x_coord in x_coords]) + sum([abs(mid_y - y_coord) for y_coord in y_coords])
