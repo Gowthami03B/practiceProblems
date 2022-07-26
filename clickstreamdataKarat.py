b005a933da3f99901f45cf8c56860c0bd51b05d1
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-07-26 12:07:06 -0400

    Create clickstreamdataKarat.py

---
+++ clickstreamdataKarat.py
@@ -0,0 +1,53 @@
+"""
+We have some clickstream data that we gathered on our client's website. Using cookies, we collected snippets of users' anonymized URL histories while they browsed the site. The histories are in chronological order, and no URL was visited more than once per person.
+
+Write a function that takes two users' browsing histories as input and returns the longest contiguous sequence of URLs that appears in both.
+
+Sample input:
+user0 = ["/start", "/green", "/blue", "/pink", "/register", "/orange", "/one/two"]
+user1 = ["/start", "/pink", "/register", "/orange", "/red", "a"]
+user2 = ["a", "/one", "/two"]
+user3 = ["/pink", "/orange", "/yellow", "/plum", "/blue", "/tan", "/red", "/amber", "/HotRodPink", "/CornflowerBlue", "/LightGoldenRodYellow", "/BritishRacingGreen"]
+user4 = ["/pink", "/orange", "/amber", "/BritishRacingGreen", "/plum", "/blue", "/tan", "/red", "/lavender", "/HotRodPink", "/CornflowerBlue", "/LightGoldenRodYellow"]
+user5 = ["a"]
+user6 = ["/pink","/orange","/six","/plum","/seven","/tan","/red", "/amber"]
+
+Sample output:
+
+findContiguousHistory(user0, user1) => ["/pink", "/register", "/orange"]
+findContiguousHistory(user0, user2) => [] (empty)
+findContiguousHistory(user0, user0) => ["/start", "/green", "/blue", "/pink", "/register", "/orange", "/one/two"]
+findContiguousHistory(user2, user1) => ["a"]
+findContiguousHistory(user5, user2) => ["a"]
+findContiguousHistory(user3, user4) => ["/plum", "/blue", "/tan", "/red"]
+findContiguousHistory(user4, user3) => ["/plum", "/blue", "/tan", "/red"]
+findContiguousHistory(user3, user6) => ["/tan", "/red", "/amber"]
+
+n: length of the first user's browsing history
+m: length of the second user's browsing history
+"""
+def clickstreamdata(user0, user1):
+    dp = [[0 for _ in range(len(user1) + 1)] for _ in range(len(user0) + 1)]
+    max_len = 0
+    res = []
+    for i in reversed(range(len(user0))):
+        for j in reversed(range(len(user1))):
+            if user0[i] == user1[j]:
+                dp[i][j] = dp[i+1][j+1] + 1
+                if dp[i][j] > max_len:
+                    max_len = dp[i][j]
+                    start = i
+                    end = i + max_len
+                    res = user0[start:end]
+                    
+    return res
+
+user0 = ["/start", "/green", "/blue", "/pink", "/register", "/orange", "/one/two"]
+user1 = ["/start", "/pink", "/register", "/orange", "/red", "a"]
+user2 = ["a", "/one", "/two"]
+user3 = ["/pink", "/orange", "/yellow", "/plum", "/blue", "/tan", "/red", "/amber", "/HotRodPink", "/CornflowerBlue", "/LightGoldenRodYellow", "/BritishRacingGreen"]
+user4 = ["/pink", "/orange", "/amber", "/BritishRacingGreen", "/plum", "/blue", "/tan", "/red", "/lavender", "/HotRodPink", "/CornflowerBlue", "/LightGoldenRodYellow"]
+user5 = ["a"]
+user6 = ["/pink","/orange","/six","/plum","/seven","/tan","/red", "/amber"]
+print(clickstreamdata(user4,user3))
+print(contiguous(user0,user1))
