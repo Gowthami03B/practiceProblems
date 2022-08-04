eedf229ff229238f760aab5667105a73804720bd
Gowthami B <gowthamibhogireddy@gmail.com>
2022-08-04 03:09:10 +0000

    Sync LeetCode submission - Longest Repeating Character Replacement (python3)

--- problems/longest_repeating_character_replacement/solution.py
+++ problems/longest_repeating_character_replacement/solution.py
@@ -1,14 +1,14 @@
-import collections
-from collections import Counter
-class Solution(object):
-    def characterReplacement(self, s, k):
-        count = collections.Counter()
-        best = i = 0
-        for j in range(len(s)):
-            count[s[j]] += 1
-            best = max(best, count[s[j]])
-            if best + k < j - i + 1:
-                count[s[i]] -= 1
-                i += 1
-        return len(s) - i
-        
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        result = 0
+        counts = defaultdict(int)
+        start = 0
+        for end in range(len(s)):
+            counts[s[end]] += 1
+            #So when the current length of the substring end-start+1 - max repeated chars > k; then we have exhausted our replacements and need to move forward hence subtract start value and start += 1
+            while end - start + 1 - max(counts.values()) > k:
+                counts[s[start]] -= 1
+                start += 1
+            result = max(result, end - start + 1)
+            
+        return result
