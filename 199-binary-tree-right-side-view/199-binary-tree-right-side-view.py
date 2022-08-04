3c321d24ec3da002e5be5bd8c6c26dc3537c9895
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-08-04 15:51:10 -0400

    Time: 52 ms (50.87%), Space: 13.9 MB (23.14%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -5,20 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+    def rightSideView1(self, root: Optional[TreeNode]) -> List[int]:
         if not root:
             return
         queue = collections.deque([(root)])
         res = []
         while queue:
             k = len(queue)
-            while k > 0:
-                k -= 1
+            while k > 0:#intuition - the nodes are the nodes that are seen last at each level
+                k -= 1#dec k
                 node = queue.popleft()
-                if k == 0:
+                if k == 0:#when k is 0 for each level, that's the last node
                     res.append(node.val)
                 if node.left:
                     queue.append((node.left))
                 if node.right:
                     queue.append((node.right))
+        return res
+    
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def helper(node, level):
+            if node is None:
+                return
+            if len(res) == level:#since we visit right node, the node we visit first in each level from right side is the right view node, hence we check if level == len(res), append to res
+                res.append(node.val)
+            helper(node.right, level + 1)#we visit right node first
+            helper(node.left, level + 1)
+        helper(root, 0)
         return res
