dcb78349082237b7b9169280fa2bee6ffb09ea26
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-08-04 11:59:02 -0400

    Time: 60 ms (17.68%), Space: 13.8 MB (65.76%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,19 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        res = []
+        res.append([1])
+        if numRows == 1:
+            return res
+        res.append([1,1])
+        if numRows == 2:
+            return res
+        while(len(res) < numRows):
+            curr = res[-1]
+            temp = []
+            temp.append(curr[0])
+            for i in range(1,len(curr)):
+                temp.append(curr[i] + curr[i-1])
+            temp.append(curr[-1])
+            res.append(temp)
+        return res
+                
