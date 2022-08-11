73039ef1bcec86f128bdd2eb97db96d7676f5fac
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-08-10 23:33:45 -0400

    Time: 57 ms (24.27%), Space: 13.9 MB (17.64%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -15,14 +15,15 @@ class Solution:
             res.append(temp)
         return res
     
+    #O(numRows^2) since outer loop takes O(numRows), inner loop runs for prev row's no of elements times, so 1+2+3...
     def generate(self, numRows: int) -> List[List[int]]:
         res = []
-        for rownum in range(numRows):
-            row = [None for _ in range(rownum + 1)]
-            row[0] = 1
+        for rownum in range(numRows):#for each no in numRows
+            row = [None for _ in range(rownum + 1)]#no of elements to rownum + 1
+            row[0] = 1#first and last are always 1
             row[-1] = 1
-            for i in range(1,len(row)-1):
-                row[i] = res[rownum-1][i-1] + res[rownum-1][i]
+            for i in range(1,len(row)-1):#loop thru rest of row
+                row[i] = res[rownum-1][i-1] + res[rownum-1][i]#prev rows i + prev row's i-1
             res.append(row)
             
         return res
