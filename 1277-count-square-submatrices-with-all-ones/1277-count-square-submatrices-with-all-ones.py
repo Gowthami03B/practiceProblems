edf08a40020502338e529b4bf14f920d914229a6
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-08-10 23:51:55 -0400

    Time: 843 ms (68.15%), Space: 16.3 MB (37.96%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -1,5 +1,21 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
+        """
+        Take an example of this matrix:
+
+        0 1 1 1
+        1 1 1 1
+        0 1 1 1
+        Maintain a running counter of length of each 2x2 square, and store the length of the square on the bottom right corner of each of those squares.
+
+        Starting at cell (1, 1), get length of the square -> min(value at (0,1), value at (1,0), value at (0,0)) + 1 = 1
+        As you navigate through the rest of the matrix, the matrix becomes:
+
+        0 1 1 1
+        1 1 2 2
+        0 1 2 3
+        
+        """
         if not matrix or len(matrix) == 0:
             return 0
         m = len(matrix)
@@ -9,6 +25,6 @@ class Solution:
             for j in range(n):
                 if matrix[i][j] == 1:
                     if i > 0 and j > 0:
-                        matrix[i][j] = min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j - 1]) + 1
-                    res += matrix[i][j]
+                        matrix[i][j] = min(matrix[i-1][j], matrix[i][j-1], matrix[i-1][j - 1]) + 1#min of up,diagonal,left values
+                    res += matrix[i][j]#count the 1's squares until now
         return res
