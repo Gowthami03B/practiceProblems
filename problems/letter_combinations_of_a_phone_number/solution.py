85af628dd07d8400a6f29ba3c6b69ba1d0b20879
Gowthami B <gowthamibhogireddy@gmail.com>
2022-08-12 20:49:29 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- problems/letter_combinations_of_a_phone_number/solution.py
+++ problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,33 +1,54 @@
+from itertools import combinations
 class Solution:
-    def letterCombinations(self, digits: str):
-        # If the input is empty, immediately return an empty answer array
-        if len(digits) == 0:
+    #O(4^N).N,O(N)
+    # That is, lock the first letter in, and solve all the possible combinations that start with that letter.
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0 or not digits:
             return []
-
-        # Map all the digits to their corresponding letters
-        letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+        mapping = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", 
                    "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
-
-        def backtrack(index, path):
-            # If the path is the same length as digits, we have a complete combination
+        def backtrack(index,path):
+             # If the path is the same length as digits, we have a complete combination
             if len(path) == len(digits):
                 combinations.append("".join(path))
-                print("Returning from base condition\n")
-                return  # Backtrack
-
-            # Get the letters that the current digit maps to, and loop through them
-            possible_letters = letters[digits[index]]
-            print("possible_letters is  %s \n" % (possible_letters))
+                return # Backtrack
+            
+            possible_letters = mapping[digits[index]]
             for letter in possible_letters:
                 # Add the letter to our current path
                 path.append(letter)
-                print("path is  %s"%(path))
                 # Move on to the next digit
-                backtrack(index + 1, path)
-                # Backtrack by removing the letter before moving onto the next
-                path.pop()
-
-        # Initiate backtracking with an empty path and starting index of 0
-        combinations = []
-        backtrack(0, [])
+                backtrack(index+1,path)
+                 # Backtrack by removing the letter before moving onto the next
+                path.pop() #say 23, after generating, ad, we pop d to generate ae,af and then pop a to get bd,be,bd, and so on
+            
+        combinations =[]
+        backtrack(0,[])
         return combinations
+
+    def letterCombinations1(self, digits: str) -> List[str]:
+        lookup = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        
+        letter_lists = []
+        for ch in digits:
+            letter_lists.append(lookup[ch])
+            
+        while len(letter_lists) > 1:
+            l2 = letter_lists.pop()
+            l1 = letter_lists.pop()
+            combos = []
+            for i in l1:
+                for j in l2:
+                    combos.append(i+j)
+            letter_lists.append(combos)
+            
+        return [] if not letter_lists else letter_lists[0]
