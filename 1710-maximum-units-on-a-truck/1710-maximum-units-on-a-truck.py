6979e75f7cea312a90f22fe901e0cb9c77a56279
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-08-23 16:13:49 -0400

    Time: 264 ms (42.55%), Space: 14.4 MB (32.32%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,9 +1,9 @@
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
-        boxTypes.sort(key = lambda x: -x[1])  
+        boxTypes.sort(key = lambda x: -x[1])  #sort by units, choose the box that has max units
         units = i = 0
         while truckSize > 0 and i < len(boxTypes):
-            boxesToPick = min(truckSize, boxTypes[i][0])
+            boxesToPick = min(truckSize, boxTypes[i][0])#how many boxes to pick is the min of truckSize and boxes
             units += boxesToPick * boxTypes[i][1]
             truckSize -= boxesToPick
             i += 1
