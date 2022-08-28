2d6709f2cd95de3a73e5d489219432f2c8e5ddf5
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-08-28 10:54:20 -0400

    Time: 688 ms (32.43%), Space: 14.8 MB (37.64%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
@@ -8,12 +8,12 @@ class Solution:
         sMap = defaultdict(map) 
         start = longest = 0
         for end in range(len(s)):
-            sMap[s[end]] = end
+            sMap[s[end]] = end #store the last index of char
             longest = max(longest, end-start)
-            if len(sMap) == 3:
-                del_indx = min(sMap.values())
+            if len(sMap) == 3:#when more than 2 distinct chars are present
+                del_indx = min(sMap.values())#find the min index char to be deleted
                 del sMap[s[del_indx]]
-                start = del_indx + 1
-        longest = max(longest, end-start+1)
+                start = del_indx + 1#start would be after the del_indx
+        longest = max(longest, end-start+1)#this is for a case when 
 
         return longest
