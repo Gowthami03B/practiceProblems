af3e2b0ed604e0a4766d86e36d77ae53d8e16aa6
Gowthami Bhogireddy <gowthamibhogireddy@gmail.com>
2022-09-01 18:16:29 -0400

    Time: 893 ms (5.07%), Space: 15.2 MB (32.19%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.py
@@ -6,5 +6,5 @@ class Solution:
                 alice_count += 1
             elif colors[i-2] == 'B' and colors[i-1] == 'B' and colors[i] == 'B':
                 bob_count += 1
-        return alice_count > bob_count
+        return alice_count > bob_count #if alice > bob, alice wins, else bob and if "AA", then both 0,hence Alice cannot make her turn hence Bob wins
                 
