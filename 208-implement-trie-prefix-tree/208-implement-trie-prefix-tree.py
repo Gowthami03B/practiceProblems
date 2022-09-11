51694e531432ead9f8cd9673f4c66e4cc419e61f
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-11 11:45:11 -0400

    Time: 327 ms (34.55%), Space: 27.4 MB (92.53%) - LeetHub

--- 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/208-implement-trie-prefix-tree.py
@@ -3,22 +3,22 @@ class Trie:
     def __init__(self):
         self.wordmap={}
 
-    def insert(self, word: str) -> None:
+    def insert(self, word: str) -> None:#O(M) -length of word to be inserted
         curr = self.wordmap
         for ch in word:
             curr= curr.setdefault(ch,{})
         curr['$'] = True
 
-    def search(self, word: str) -> bool:
+    def search(self, word: str) -> bool:#O(M) -length of word to be searched
         curr = self.wordmap
         for ch in word:
             if ch in curr:
                 curr = curr[ch]
             else:
                 return False
-        return '$' in curr
+        return True if '$' in curr else False
 
-    def startsWith(self, prefix: str) -> bool:
+    def startsWith(self, prefix: str) -> bool:#O(M) -length of prefix to be searched
         curr = self.wordmap
         for ch in prefix:
             if ch in curr:
