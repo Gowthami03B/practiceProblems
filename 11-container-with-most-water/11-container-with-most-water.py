759f4a238e709f02cc3ce4b48f9d69b1d5fcb1c0
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-14 17:29:35 -0400

    Time: 2104 ms (5.00%), Space: 27.6 MB (12.08%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,10 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        #Two pointers
         i , j = 0, len(height) - 1
         maxvolume = float('-inf')
+        # We take two pointers, one at the beginning and one at the end of the array constituting the length of the lines. Futher, we maintain a variable maxarea to store the maximum area obtained till now. At every step, we find out the area formed between them, update maxarea
         while(i < j):
             maxvolume = max(maxvolume, min(height[i],height[j])* (j-i))
-            if height[i] < height[j]:
+            if height[i] < height[j]:#to maximize the area, we choose to keep the line with higher height
                 i += 1
             elif(height[i] >= height[j]):
                 j -= 1
