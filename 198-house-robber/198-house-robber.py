411c2867f055e475b3302e17b21de35eb23cf2e5
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-15 09:04:55 -0400

    Time: 65 ms (15.23%), Space: 13.8 MB (65.27%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -4,9 +4,10 @@ class Solution:
         if n == 1:
             return nums[0]
         dp = [0] * n
+        #base cases
         dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
+        dp[1] = max(nums[0],nums[1])#for 2nd house, u can rob it if it's max or 1st house
         for i in range(2, len(nums)):
-            dp[i] = max(dp[i-1],nums[i] + dp[i-2])
-        return max(dp)
+            dp[i] = max(dp[i-1],nums[i] + dp[i-2])#for all others, its the max of prev until now, nums[i] + dp[i-2]
+        return dp[-1]
         
