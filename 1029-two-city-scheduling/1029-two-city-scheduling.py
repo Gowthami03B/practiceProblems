ba43c7f543a112fe91fbb91ea573fa70da1cea28
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-15 20:54:28 -0400

    Time: 64 ms (58.01%), Space: 14.1 MB (10.36%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.py
+++ 1029-two-city-scheduling/1029-two-city-scheduling.py
@@ -1,6 +1,6 @@
 from heapq import heappush,heappop
 class Solution:
-    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+    def twoCitySchedCost1(self, costs: List[List[int]]) -> int:
             countA, countB, tot_len, total_cost = 0, 0, len(costs)//2, 0
             heap = []
             for cost in costs:
@@ -20,3 +20,16 @@ class Solution:
             index = 0 if countA < countB else 1
             total_cost += sum([ele[1][index] for ele in heap])
             return total_cost
+        
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        # we sort and after this we get the lowest costs associated with city A first
+        costs.sort(key = lambda x : x[0] - x[1])
+        print(costs)
+        total = 0
+        n = len(costs) // 2
+        # To optimize the company expenses,
+        # send the first n persons to the city A (lowest cost for A)
+        # and the others to the city B
+        for i in range(n):
+            total += costs[i][0] + costs[i + n][1]
+        return total
