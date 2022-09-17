07dd4f36e39fd055e48b3a45f403bc518c5e9fac
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-17 11:48:48 -0400

    Time: 36 ms (87.54%), Space: 13.8 MB (97.94%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -10,11 +10,11 @@ class Solution:
         #if 3 houses, since its a circle, then middle house or max(first and last)
         return max(self.rob_simple(nums[:-1]), self.rob_simple(nums[1:]))
                    
-    def rob_simple(self,nums):
+    def rob_simple(self,nums):#[1,3,1,3,100], [1,3,1,3] or [3,1,3,100], 3,3 or 1,100 or 3,100
         dp =[0] * len(nums)
         dp[0] = nums[0]
         dp[1] = nums[1]
         for i in range(2,len(nums)):
-            dp[i] = max(dp[i-2],nums[i]+dp[i-2],nums[i]+dp[i-3])
+            dp[i] = max(dp[i-2],nums[i]+dp[i-2],nums[i]+dp[i-3])#max(last to last, curr + last to last, curr + last to last to last)
         return max(dp)
         
