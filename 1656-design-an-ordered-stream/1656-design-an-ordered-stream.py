fb94fd54944a7299f89af63c8fef2b95bcfae51f
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-18 09:47:54 -0400

    Time: 652 ms (5.12%), Space: 15 MB (10.33%) - LeetHub

---
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -0,0 +1,23 @@
+class OrderedStream:
+
+    def __init__(self, n: int):
+        self.stream_set = set()
+        self.stream={}
+        self.last_added = -1
+
+    def insert(self, idKey: int, value: str) -> List[str]:
+        self.stream[idKey] = value
+        self.stream_set.add(idKey)
+        res= []
+        i = idKey - 1
+        if self.last_added == idKey - 1 or (self.last_added == -1 and idKey ==1):
+            while i+1 in self.stream_set:
+                res.append(self.stream[i+1])
+                self.last_added = i+1
+                i += 1
+        return res
+
+
+# Your OrderedStream object will be instantiated and called as such:
+# obj = OrderedStream(n)
+# param_1 = obj.insert(idKey,value)
