db1f484d5090e5956a29e69003ad3d3761d09fd1
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-24 10:58:50 -0400

    Time: 30 ms (94.14%), Space: 14 MB (20.05%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,6 +1,6 @@
 
 class Solution:
-    def decodeString(self, s: str) -> str:
+    def decodeString1(self, s: str) -> str:
         stack = []
         temp_string = []
         repeat_string= ""
@@ -26,4 +26,29 @@ class Solution:
                 repeat_string= ""
                 final_string=""
                 k = ""
+        return "".join(stack)
+    
+    def decodeString(self, s: str) -> str:
+        stack = []
+        for i in range(len(s)):
+            if s[i] != ']':
+                stack.append(s[i])
+            else:
+                temp_string = []
+                k = []
+                while stack and stack[-1] != '[':
+                    temp_string.append(stack.pop())
+                # print(temp_string)
+                if stack[-1] == '[':
+                    stack.pop()#pop open bracket
+                while stack and stack[-1].isdigit():
+                    k.append(stack.pop())#pop digit
+                temp_string.reverse()
+                k.reverse()
+                k = int("".join(k))
+                temp_string *= k
+                # print(temp_string)
+                for c in temp_string:
+                    stack.append(c)
+
         return "".join(stack)
