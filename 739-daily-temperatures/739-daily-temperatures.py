04f7689b18ed2b5256edf9151e27385d92be8ff4
Gowthami B <gowthamibhogireddy@gmail.com>
2022-09-25 10:57:02 -0400

    Time: 1207 ms (94.92%), Space: 24.7 MB (72.23%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -3,10 +3,10 @@ class Solution:
         n = len(temperatures)
         answer = [0] * n
         stack = []
-        
+         # Pop until the current day's temperature is not warmer than the temperature at the top of the stack
+            # On each day, there are two possibilities. If the current day's temperature is not warmer than the temperature on the top of the stack, we can just push the current day onto the stack - since it is not as warm (equal or smaller), this will maintain the sorted property (decreasing stack).
+            # If the current day's temperature is warmer than the temperature on top of the stack, this is significant. It means that the current day is the first day with a warmer temperature than the day associated with the temperature on top of the stack. Hence we pop and diff between indices is the no of days for a warmer temp
         for curr_day, curr_temp in enumerate(temperatures):
-            # Pop until the current day's temperature is not
-            # warmer than the temperature at the top of the stack
             while stack and temperatures[stack[-1]] < curr_temp:
                 prev_day = stack.pop()
                 answer[prev_day] = curr_day - prev_day
