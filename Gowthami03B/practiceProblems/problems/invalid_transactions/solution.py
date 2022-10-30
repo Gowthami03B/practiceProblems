9ee35af11f14a4806cdca02857fd14b95a16b5a7
Gowthami B <gowthamibhogireddy@gmail.com>
2022-10-30 22:52:07 +0000

    Sync LeetCode submission - Invalid Transactions (python3)

--- Gowthami03B/practiceProblems/problems/invalid_transactions/solution.py
+++ Gowthami03B/practiceProblems/problems/invalid_transactions/solution.py
@@ -1,29 +1,71 @@
+from collections import defaultdict
 class Solution:
-    def invalidTransactions(self, transactions):
-        hashmap = collections.defaultdict(list)
-		#We will only store index of the transaction because the same transaction can repeat.
-        result = set()
-        for i, t in enumerate(transactions):
-            name, time, amount, city =  t.split(",")
-			#If there is no transaction record for the user
-            if name not in hashmap:
-                if int(amount) > 1000:
-                    result.add(i)
-			#If there are old tx records
-            else:
-				#Fetching past transaction from hashmap
-                prevTrans = hashmap[name]
-				#Iterating over the past transaction of the user and finding anomalies
-                for j in range(len(prevTrans)):
-                    prevName, prevTime, prevAmount, prevCity = transactions[prevTrans[j]].split(",")
-                    if int(amount) > 1000:
-                        result.add(i)
-				#Checking whether it occurs within (and including) 60 min and diff city
-                    if abs(int(time) - int(prevTime)) <= 60 and city != prevCity:
-                        result.add(i)
-                        result.add(prevTrans[j])
-			#Recording transaction in the hashmap for the user
-            hashmap[name].append(i)
-		
-		#Fetching transaction using indexes stored in the result set and returning
-        return [transactions[t] for t in result]
+    def invalidTransactions1(self, transactions: List[str]) -> List[str]:
+        transaction_map = defaultdict(list)
+        invalid = []
+        
+        for tx in transactions:
+            name, time, amount, city = tx.split(",")
+            if int(amount) > 1000:
+                invalid.append(tx)
+            if name in transaction_map:
+                flag = False
+                for prev in transaction_map[name]:
+                    p_name, p_time, p_amount, p_city = prev.split(",")
+                    if p_city != city and abs(int(p_time)- int(time)) < 60:
+                        flag=True
+                        invalid.append(prev)
+                    
+                if flag:
+                    invalid.append(tx)                
+            
+            transaction_map[name].append(tx)
+            
+        print(invalid)
+        return invalid
+    
+    def invalidTransactions1(self, arr: List[str]) -> List[str]:
+        self.memo = collections.defaultdict(list)
+        ans = set()
+        for s in arr:
+            name, mi, mon, place = s.split(",")
+            if int(mon) > 1000:
+                ans.add(s)
+            if self.memo[name]:
+                flag = False
+                for a,b,c,d in self.memo[name]:
+                    if d != place and abs(int(mi) - b) <= 60:
+                        flag = True
+                        ans.add(a + "," + str(b) + "," + str(c) + "," + d)
+                if flag:
+                    ans.add(s)
+            self.memo[name] += ((name, int(mi), int(mon), place)),
+        return ans
+    
+    def invalidTransactions(self, transactions: List[str]) -> List[str]:
+        if len(transactions) == 1: return transactions
+        transactions = sorted(transactions)
+        print(transactions)
+        n = len(transactions)
+        invalid = set()
+
+        for i in range(n):
+            name1, time1, amount1, city1 = transactions[i].split(',')
+            if int(amount1) > 1000:
+                invalid.add(i)
+
+            for j in range(i+1, n):
+                name2, time2, amount2, city2 = transactions[j].split(',')
+                if name1 != name2:
+                    break
+                if int(amount2) > 1000:   
+                    invalid.add(j)
+                if city1 != city2 and abs(int(time2) - int(time1)) <= 60:
+                    invalid.add(i)
+                    invalid.add(j) 
+
+        result = []
+        for i, transaction in enumerate(transactions): 
+            if invalid.__contains__(i):
+                result.append(transaction)
+        return result
