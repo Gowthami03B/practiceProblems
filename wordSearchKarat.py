1f87bb491117652035b462259cfa89396765dc89
Gowthami B <gowthamibhogireddy@gmail.com>
2022-11-21 21:46:18 -0500

    Update wordSearchKarat.py

--- wordSearchKarat.py
+++ wordSearchKarat.py
@@ -10,7 +10,7 @@ ex - grid = [
 word = "cccc", output = [(0,0), (1,0), (2, 0), (3, 0)]
 word = "cccab", output = [(0,0), (1,0), (2, 0), (2, 1), (2, 2)] or [(0,0), (1,0), (1, 1), (2, 1), (2, 2)]
 """
-def wordSearchKarat(grid, word):
+def wordSearchKarat1(grid, word):
     rows = len(grid)
     cols = len(grid[0])
     res = []
@@ -26,6 +26,30 @@ def wordSearchKarat(grid, word):
         for j in range(cols):
             dfs(i,j,0,[])
     return res
+
+def wordSearchKarat(grid,word):
+    dir = [[0,1],[1,0]]
+    rows = len(grid)
+    cols = len(grid[0])
+    coordinates = []
+    def bfs(r,c,index):
+        if index == len(word):
+            return True
+        if r <0  or c <0 or r==rows or c == cols or grid[r][c] != word[index]:
+            return False
+        coordinates.append((r,c))
+        for dr,dc in dir:
+            nr,nc = r+dr, c+dc
+            if bfs(nr,nc,index+ 1):
+                return True
+        coordinates.pop()
+        return False
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == word[0]:
+                if bfs(i,j,0):
+                    return coordinates
 
 
 grid = [
